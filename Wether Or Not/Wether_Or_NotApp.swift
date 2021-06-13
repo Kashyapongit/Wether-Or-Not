2a89f7b27c4300cbfc361427d1827d58dcb30555
Sachin <kashyap7024@gmail.com>
2021-06-13 17:44:03 +0530

    Initial Commit

---
+++ Wether Or Not/Wether_Or_NotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wether_Or_NotApp.swift
+//  Wether Or Not
+//
+//  Created by Sachin Kashyap on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Wether_Or_NotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
